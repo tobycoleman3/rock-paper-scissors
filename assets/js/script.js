21ece845a2e75afe5ee2d2ddb4a8ada470bacff6
toby coleman <tobycoleman3@hotmail.co.uk>
2024-04-18 09:08:04 +0000

    add javascript to open and close the modal rules window that pops up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("connected!")
+// get the modal
+var modal = document.getElementById('rulesModal');
+// get the button that opens the modal
+var btn = document.getElementById('rulesBtn');
+// get the span element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+// when the user clicks the button it opens the modal
+btn.onclick = function() {
+  modal.style.display = "block"
+}
+// when the user clocks on span closes the modal 
+span.onclick = function() {
+  modal.style.display = "none"
+}
+// when the user clicks anywhere else closes the modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none"
+  }
+}
