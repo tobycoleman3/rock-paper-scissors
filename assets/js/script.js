696d8b9da2fd60ccf308ae8ca15349d0c2cfa716
toby coleman <tobycoleman3@hotmail.co.uk>
2024-05-30 07:48:37 +0000

    formatted documents

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,15 @@ var btn = document.getElementById('rulesBtn');
 // get the span element that closes the modal
 var span = document.getElementsByClassName('close')[0];
 // when the user clicks the button it opens the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block"
 }
 // when the user clocks on span closes the modal 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none"
 }
 // when the user clicks anywhere else closes the modal
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none"
   }
@@ -23,8 +23,8 @@ let playerScore = 0;
 let computerScore = 0;
 let moves = 0;
 
- // Function to
- const playGame = () => {
+// Function to
+const playGame = () => {
   const rockBtn = document.querySelector('.rock');
   const paperBtn = document.querySelector('.paper');
   const scissorsBtn = document.querySelector('.scissors');
@@ -56,8 +56,8 @@ let moves = 0;
   })
 }
 
- // How to decide winner
- const winner = (player, computer) => {
+// How to decide winner
+const winner = (player, computer) => {
   const result = document.querySelector('.result');
   console.log("results:", result)
   const playerScoreBoard = document.querySelector('.p-count');
@@ -179,8 +179,8 @@ let moves = 0;
   }
 }
 
- // Funtion to run when game is over
- const gameOver = (playerOptions, movesLeft) => {
+// Funtion to run when game is over
+const gameOver = (playerOptions, movesLeft) => {
   const chooseMove = document.querySelector('.move');
   const result = document.querySelector('.result');
   const reload = document.querySelector('.reload');
